a59b67aad8569675d21a83b154af3bee91f94aa0
Abinash-Adhikari <77550193+Abinash-Adhikari@users.noreply.github.com>
2021-06-05 19:15:56 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import './App.css';
+function App() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+        <Note />
+        <Note />
+        <Note />
+      </div>
+    );
+}
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+function Note() {
+    return (
+       <div className='note'>
+           <h1> Javascript and React.js</h1>
+           <p>
+               This was amazing bootcamp taken up by Saurya Sinha. 
+               We covered everithings from Scartch including javascript React.js, HTML.
+           </p>
+       </div>
+    );
+}
+export default Note;
